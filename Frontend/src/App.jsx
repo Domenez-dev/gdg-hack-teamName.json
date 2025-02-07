4d17fdd2d410dc977ce4ff4ec185001631ee3571
Cerinegoumraca <155079290+Cerinegoumraca@users.noreply.github.com>
2025-02-07 23:06:22 +0100

    login page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,12 @@
-import HR from "./pages/HR";
+import Dashboard from "./pages/Dashboard";
 import Events from "./pages/Events";
+import LoginPage from "./pages/Login";
 function App() {
   return (
    
     
         <div className="p-6">
-          <HR />
+          <LoginPage/>
         </div>
       );
     }
---
+++ Frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,29 @@
+import LeaderBoard from "../components/LeaderBoard";
+import MemberList from "../components/Member-list";
+
+
+
+
+import LeftSidebar from "../components/LeftSidebar";
+
+function Dashboard() {
+  
+
+  return (
+
+  
+  <div className="p-6 max-w-6xl mx-auto space-y-8">
+     <LeftSidebar />
+    <LeaderBoard />
+    <MemberList/> 
+    
+
+
+      
+      
+
+    </div>
+  );
+}
+
+export default Dashboard;
