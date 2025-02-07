fd0667c91f2753de8256e1e5b524b6563686d882
Cerinegoumraca <155079290+Cerinegoumraca@users.noreply.github.com>
2025-02-07 14:44:58 +0100

    member list hr

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import HR from './pages/Hr'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 class="text-3xl text-red-500 font-bold underline">
-    Hello world!
-  </h1>
+    <div className="p-6">
+     
+      <HR />
+  </div>
   )
 }
 
---
+++ Frontend/src/pages/Hr.jsx
@@ -0,0 +1,11 @@
+import MemberList from "../components/Member-list";
+function HR() {
+  return (
+    <div className="p-6">
+     
+      <MemberList />
+    </div>
+  );
+}
+
+export default HR;
