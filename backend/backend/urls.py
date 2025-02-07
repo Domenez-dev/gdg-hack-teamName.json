24ebc5e40eb471af16a5059a0755cb1200d21251
Nooorrrr <noor.bnhdd@gmail.com>
2025-02-07 12:22:56 +0100

    events

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     path('admin/', include('users.urls')), # Include the users app urls
     path('api/', include('feedback.urls')), # Include the users app urls
+    path('api/', include('events.urls')), # Include the users app urls
 ]
--- backend/events/models.py
+++ backend/events/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    id = models.AutoField(primary_key=True)
+    event_name = models.CharField(max_length=255)
+    event_start_date = models.DateTimeField()
+    event_end_date = models.DateTimeField()
+    event_description = models.TextField()
+
+    def __str__(self):
+        return str(self.name)
