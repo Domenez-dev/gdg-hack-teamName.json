1a2d02c0be8b5df5cc8f5fc94ee6ed4b0521e3b1
Cerinegoumraca <155079290+Cerinegoumraca@users.noreply.github.com>
2025-02-07 20:16:19 +0100

    Formulaire pour ajouter des events

--- Frontend/src/pages/Hr.jsx
+++ Frontend/src/pages/Hr.jsx
@@ -1,20 +1,22 @@
 import LeaderBoard from "../components/LeaderBoard";
 import MemberList from "../components/Member-list";
-
+import AddNewForm from "../components/Add";
+import { useState } from "react";
 function HR() {
+  const [isOpen,setisOpen]=useState(false)
+
   return (
 
   
-  <div className="p-6 max-w-6xl mx-auto space-y-8">
+  <div className="p-6 max-w-6xl mx-auto space-y-8 ">
       {/* HR OVERVIEW - Prend toute la largeur */}
       <h2 className="text-2xl text-white-600 font-bold text-left w-full">
         HR OVERVIEW
       </h2>
+      <button onClick={()=> setisOpen(true)}>ADD</button>
       {/* LeaderBoard au-dessus */}
-      <LeaderBoard />
+      {isOpen && <AddNewForm onClose={()=> setisOpen(false)}/>}
 
-      {/* MemberList en dessous */}
-      <MemberList />
     </div>
   );
 }
